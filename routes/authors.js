c16ede6e478a0a2a3338addbc219b8f8afeddb68
ct <821982911@qq.com>
2023-01-12 15:35:21 +0800

    finnish authors

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Author = require("../models/author");
+const Book = require("../models/book");
 
 // All Authors Route
 router.get("/", async (req, res) => {
@@ -35,13 +36,83 @@ router.post("/", async (req, res) => {
 
   try {
     const newAuthor = await author.save();
-    // res.redirect(`authors/${newAuthor.id}`);
-    res.redirect("authors");
+    res.redirect(`authors/${newAuthor.id}`);
   } catch (error) {
     res.render("authors/new", {
       author,
       errorMessage: "Error Creating Author",
     });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const author = await Author.findById(id);
+    const books = await Book.find({ author: author.id }).limit(6).exec();
+
+    res.render("authors/show", {
+      author,
+      booksByAuthor: books,
+    });
+  } catch (error) {
+    res.redirect("/");
+  }
+});
+
+router.get("/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const author = await Author.findById(id);
+    res.render("authors/edit", { author });
+  } catch (error) {
+    res.redirect("/authors");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  let author;
+  try {
+    author = await Author.findById(id);
+
+    author.name = name;
+    await author.save();
+    res.redirect(`/authors/${author.id}`);
+  } catch (error) {
+    if (!author) {
+      // 没查找到
+      res.redirect("/");
+      return;
+    }
+
+    // 保存失败 回去继续编辑
+    res.render("authors/edit", {
+      author,
+      errorMessage: "Error Updating Author",
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  let author;
+  try {
+    author = await Author.findById(id);
+    await author.remove();
+    res.redirect("/authors");
+  } catch (error) {
+    if (!author) {
+      // 没查找到
+      res.redirect("/");
+      return;
+    }
+
+    // 保存失败 回去继续编辑
+    res.redirect(`/authors/${author.id}`);
   }
 });
 
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
@@ -18,6 +19,7 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
+app.use(methodOverride("_method"));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: false }));
 
