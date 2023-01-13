cdf2395ca6773026029223c4ae2751ab1e28fe56
ct <821982911@qq.com>
2023-01-13 19:36:31 +0800

    finshed css

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,15 +1,32 @@
-FilePond.registerPlugin(
-  FilePondPluginImagePreview,
-  FilePondPluginImageResize,
-  FilePondPluginFileEncode,
-);
+const rootStyles = window.getComputedStyle(document.documentElement);
+if (rootStyles.getPropertyValue("--book-cover-width-large")) {
+  ready();
+} else {
+  document.getElementById("main-css").addEventListener("load", ready);
+}
 
-FilePond.setOptions({
-  // 这里的比例是 展示图片的比例
-  stylePanelAspectRatio: 150 / 100,
-  // 更改 上传上服务器的宽高
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150,
-});
+function ready() {
+  const coverWidth = parseFloat(
+    rootStyles.getPropertyValue("--book-cover-width-large"),
+  );
+  const coverAspectRatio = parseFloat(
+    rootStyles.getPropertyValue("--book-cover-aspect-ratio"),
+  );
+  const coverHeight = coverWidth / coverAspectRatio;
 
-FilePond.parse(document.body);
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+  );
+
+  FilePond.setOptions({
+    // 这里的比例是 展示图片的比例
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    // 更改 上传上服务器的宽高
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150,
+  });
+
+  FilePond.parse(document.body);
+}
